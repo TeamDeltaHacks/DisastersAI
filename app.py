3b7f9859b4643ecf8070ca3d49fb4067f4be49e0
Kabir R <contact@kabirwearsato.ga>
2021-01-16 11:38:06 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
-@app.route('/fires')
-def features():
-    return render_template('fires.html')
+@app.route('/fires', methods=['GET', 'POST'])
+def fires():
+	if request.method == 'POST':
+		print(request.form)
+		return render_template('fires.html')
+	else:
+		return render_template('fires.html')
 
-@app.route('/hurricanes')
-def pricing():
-    return render_template('hurricanes.html')
+@app.route('/hurricanes', methods=['GET', 'POST'])
+def hurricanes():
+	if request.method == 'POST':
+		print(request.form)
+		return render_template('hurricanes.html')
+	else:
+		return render_template('hurricanes.html')
 
-@app.route('/earthquakes')
-def blog():
-    return render_template('earthquakes.html')
+@app.route('/earthquakes', methods=['GET', 'POST'])
+def earthquakes():
+	if request.method == 'POST':
+		print(request.form)
+		return render_template('earthquakes.html')
+	else:
+		return render_template('earthquakes.html')
